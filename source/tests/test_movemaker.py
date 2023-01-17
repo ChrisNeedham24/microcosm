02f0003dbfb1ff1bd5f3184bcf560edf9141ff76
ChrisNeedham24 <christopherneedham24@gmail.com>
2023-01-17 20:47:02 +1100

    MCR-114 Added some movemaker tests.

--- source/tests/test_movemaker.py
+++ source/tests/test_movemaker.py
@@ -2,9 +2,10 @@ import unittest
 from unittest.mock import patch
 
 from source.display.board import Board
-from source.foundation.catalogue import Namer, UNIT_PLANS
-from source.foundation.models import GameConfig, Faction, Unit, Player, Settlement
-from source.game_management.movemaker import search_for_relics_or_move
+from source.foundation.catalogue import Namer, UNIT_PLANS, BLESSINGS, get_unlockable_improvements
+from source.foundation.models import GameConfig, Faction, Unit, Player, Settlement, AIPlaystyle, AttackPlaystyle, \
+    ExpansionPlaystyle, Blessing
+from source.game_management.movemaker import search_for_relics_or_move, set_blessing
 
 
 class MovemakerTest(unittest.TestCase):
@@ -16,7 +17,8 @@ class MovemakerTest(unittest.TestCase):
     TEST_UNIT = Unit(1, UNIT_PLANS[0].total_stamina, (3, 4), False, UNIT_PLANS[0])
     TEST_UNIT_2 = Unit(5, 6, (7, 8), False, UNIT_PLANS[0])
     TEST_UNIT_3 = Unit(9, 10, (11, 12), False, UNIT_PLANS[0])
-    TEST_PLAYER = Player("TesterMan", Faction.NOCTURNE, 0, 0, [], [TEST_UNIT], [], set(), set())
+    TEST_PLAYER = Player("TesterMan", Faction.NOCTURNE, 0, 0, [], [TEST_UNIT], [], set(), set(),
+                         ai_playstyle=AIPlaystyle(AttackPlaystyle.NEUTRAL, ExpansionPlaystyle.NEUTRAL))
     TEST_SETTLEMENT = Settlement("Obstructionville", (0, 0), [], [], [])
 
     def setUp(self) -> None:
@@ -46,20 +48,120 @@ class MovemakerTest(unittest.TestCase):
         self.TEST_UNIT_3.location = self.relic_coords[0], self.relic_coords[1] + 1
         self.TEST_SETTLEMENT.location = self.relic_coords[0], self.relic_coords[1] - 1
         self.TEST_PLAYER.units = [self.TEST_UNIT]
+        self.TEST_PLAYER.blessings = []
+        self.TEST_PLAYER.ongoing_blessing = None
+        self.TEST_PLAYER.ai_playstyle = AIPlaystyle(AttackPlaystyle.NEUTRAL, ExpansionPlaystyle.NEUTRAL)
 
-    """
-    Set blessing cases to test
+    def test_set_blessing_none_available(self):
+        """
+        Ensure that an AI player's blessing is not set if there are none available, i.e. they have undergone all of
+        them.
+        """
+        # Give the player every blessing.
+        self.TEST_PLAYER.blessings = BLESSINGS.values()
 
-    No available blessings
-    Player requires wealth
-    Player requires harvest
-    Player requires zeal
-    Player requires fortune
-    Aggressive AI chooses unit
-    Aggressive AI no units, takes ideal
-    Defensive AI chooses strength
-    Defensive AI no strength, takes ideal
-    """
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (0, 0, 0, 0))
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+
+    def test_set_blessing_wealth(self):
+        """
+        Ensure that an AI player's blessing is set to the best possible one for wealth if that is what they are lacking.
+        """
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (0, 1, 1, 1))
+        # The blessing that yields the most wealth is Self-Locking Vaults, with 29.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["sl_vau"], new_blessing)
+
+    def test_set_blessing_harvest(self):
+        """
+        Ensure that an AI player's blessing is set to the best possible one for harvest if that is what they are
+        lacking.
+        """
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (1, 0, 1, 1))
+        # The blessing that yields the most harvest is Self-Locking Vaults, with 25.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["sl_vau"], new_blessing)
+
+    def test_set_blessing_zeal(self):
+        """
+        Ensure that an AI player's blessing is set to the best possible one for zeal if that is what they are lacking.
+        """
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (1, 1, 0, 1))
+        # The blessing that yields the most zeal is Robotic Experiments, with 32.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["rob_exp"], new_blessing)
+
+    def test_set_blessing_fortune(self):
+        """
+        Ensure that an AI player's blessing is set to the best possible one for fortune if that is what they are
+        lacking.
+        """
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (1, 1, 1, 0))
+        # The blessing that yields the most fortune is Divine Architecture, with 30.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["div_arc"], new_blessing)
+
+    def test_set_blessing_aggressive_unit(self):
+        """
+        Ensure than an aggressive AI player's blessing is set to the first one that unlocks a unit.
+        """
+        self.TEST_PLAYER.ai_playstyle.attacking = AttackPlaystyle.AGGRESSIVE
+
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (0, 0, 0, 0))
+        # The first blessing that yields a unit is Beginner Spells.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["beg_spl"], new_blessing)
+
+    def test_set_blessing_aggressive_no_units(self):
+        """
+        Ensure than an aggressive AI player's blessing is set to the best possible one for wealth if that is what they
+        are lacking and there are no remaining blessings that unlock units.
+        """
+        self.TEST_PLAYER.ai_playstyle.attacking = AttackPlaystyle.AGGRESSIVE
+        # Technically this will result in duplicates in the list, but that doesn't matter for our purposes.
+        self.TEST_PLAYER.blessings = [up.prereq for up in UNIT_PLANS if up.prereq is not None]
+
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (0, 1, 1, 1))
+        # The blessing that yields the most wealth excluding those that yield units is Economic Movements, with 20.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["eco_mov"], new_blessing)
+
+    def test_set_blessing_defensive_strength(self):
+        """
+        Ensure than an defensive AI player's blessing is set to the first one that unlocks an improvement that yields
+        strength.
+        """
+        self.TEST_PLAYER.ai_playstyle.attacking = AttackPlaystyle.DEFENSIVE
+
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (0, 0, 0, 0))
+        # The first blessing that yields an improvement with strength is Rudimentary Explosives.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["rud_exp"], new_blessing)
+
+    def test_set_blessing_defensive_no_strength(self):
+        """
+        Ensure than a defensive AI player's blessing is set to the best possible one for harvest if that is what they
+        are lacking and there are no remaining blessings that unlock improvements with strength.
+        """
+        self.TEST_PLAYER.ai_playstyle.attacking = AttackPlaystyle.DEFENSIVE
+        # Give the player all the blessings that unlock improvements with strength.
+        self.TEST_PLAYER.blessings = [bls for bls in BLESSINGS.values()
+                                      if [imp for imp in get_unlockable_improvements(bls) if imp.effect.strength > 0]]
+
+        self.assertIsNone(self.TEST_PLAYER.ongoing_blessing)
+        set_blessing(self.TEST_PLAYER, (1, 0, 1, 1))
+        # The blessing that yields the most harvest excluding those that yield strength is Metabolic Alterations, with
+        # 15.
+        new_blessing: Blessing = self.TEST_PLAYER.ongoing_blessing.blessing
+        self.assertEqual(BLESSINGS["met_alt"], new_blessing)
 
     @patch("source.game_management.movemaker.investigate_relic", lambda *args: None)
     def test_search_for_relics_success_left(self):
