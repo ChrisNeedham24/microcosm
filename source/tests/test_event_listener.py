0d5e56cdc27b97d28fd103d1a10afb847e10f89e
ChrisNeedham24 <christopherneedham24@gmail.com>
2024-08-07 21:23:12 +1000

    MCR-117 Added a couple more event listener tests.

--- source/tests/test_event_listener.py
+++ source/tests/test_event_listener.py
@@ -1,10 +1,16 @@
+import json
 import socket
 import unittest
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 
-from source.foundation.models import PlayerDetails, Faction
+from source.foundation.catalogue import LOBBY_NAMES, PLAYER_NAMES, FACTION_COLOURS
+from source.foundation.models import PlayerDetails, Faction, GameConfig, Player
+from source.game_management.game_state import GameState
 from source.networking.event_listener import RequestHandler, MicrocosmServer
-from source.networking.events import EventType, RegisterEvent
+from source.networking.events import EventType, RegisterEvent, Event, CreateEvent, InitEvent, UpdateEvent, \
+    UpdateAction, QueryEvent, LeaveEvent, JoinEvent, EndTurnEvent, UnreadyEvent, AutofillEvent, SaveEvent, \
+    QuerySavesEvent, LoadEvent
+from source.saving.save_encoder import SaveEncoder
 
 
 class EventListenerTest(unittest.TestCase):
@@ -20,6 +26,7 @@ class EventListenerTest(unittest.TestCase):
     TEST_EVENT: RegisterEvent = RegisterEvent(EventType.REGISTER, TEST_IDENTIFIER, TEST_PORT)
     TEST_EVENT_BYTES: bytes = b'{"type":"REGISTER","identifier":123,"port":9999}'
     TEST_GAME_NAME: str = "My favourite game"
+    TEST_GAME_CONFIG: GameConfig = GameConfig(2, Faction.AGRICULTURISTS, True, True, True, True)
 
     def setUp(self):
         """
@@ -36,6 +43,9 @@ class EventListenerTest(unittest.TestCase):
             self.TEST_IDENTIFIER: (self.TEST_HOST, self.TEST_PORT),
             self.TEST_IDENTIFIER_2: (self.TEST_HOST_2, self.TEST_PORT_2)
         }
+        self.mock_server.namers_ref = {}
+        self.mock_server.move_makers_ref = {}
+        self.mock_server.lobbies_ref = {}
         self.request_handler: RequestHandler = RequestHandler((self.TEST_EVENT_BYTES, self.mock_socket),
                                                               (self.TEST_HOST, self.TEST_PORT), self.mock_server)
 
@@ -84,6 +94,98 @@ class EventListenerTest(unittest.TestCase):
         self.request_handler._forward_packet(self.TEST_EVENT, self.TEST_GAME_NAME, self.mock_socket,
                                              gate=lambda pd: False)
         self.mock_socket.sendto.assert_not_called()
+
+    def test_process_event(self):
+        """
+        Ensure that events are correctly assigned to the correct process method based on their type.
+        """
+        def validate_event_type(event: Event, expected_method: str, with_sock: bool = True):
+            """
+            Ensure that the given event, when processed, calls the expected process method.
+            :param event: The event to process and validate.
+            :param expected_method: The name of the method we expect to be called in the request handler when processing
+                                    the given event.
+            :param with_sock: Whether we expect the socket to be included as an argument in the call to the expected
+                              method.
+            """
+            # Mock out the method.
+            self.request_handler.__setattr__(expected_method, MagicMock())
+            self.request_handler.process_event(event, self.mock_socket)
+            expected_args: list = [event, self.mock_socket] if with_sock else [event]
+            # Ensure the method was called with the expected arguments.
+            self.request_handler.__getattribute__(expected_method).assert_called_with(*expected_args)
+
+        # Go through each event type.
+        validate_event_type(CreateEvent(EventType.CREATE, self.TEST_IDENTIFIER, self.TEST_GAME_CONFIG),
+                            "process_create_event")
+        validate_event_type(InitEvent(EventType.INIT, self.TEST_IDENTIFIER, self.TEST_GAME_NAME), "process_init_event")
+        validate_event_type(UpdateEvent(EventType.UPDATE, self.TEST_IDENTIFIER, UpdateAction.FOUND_SETTLEMENT,
+                                        self.TEST_GAME_NAME, Faction.AGRICULTURISTS), "process_update_event")
+        validate_event_type(QueryEvent(EventType.QUERY, self.TEST_IDENTIFIER), "process_query_event")
+        validate_event_type(LeaveEvent(EventType.LEAVE, self.TEST_IDENTIFIER, self.TEST_GAME_NAME),
+                            "process_leave_event")
+        validate_event_type(JoinEvent(EventType.JOIN, self.TEST_IDENTIFIER,
+                                      self.TEST_GAME_NAME, Faction.AGRICULTURISTS), "process_join_event")
+        validate_event_type(RegisterEvent(EventType.REGISTER, self.TEST_IDENTIFIER, self.TEST_PORT),
+                            "process_register_event", with_sock=False)
+        validate_event_type(EndTurnEvent(EventType.END_TURN, self.TEST_IDENTIFIER, self.TEST_GAME_NAME),
+                            "process_end_turn_event")
+        validate_event_type(UnreadyEvent(EventType.UNREADY, self.TEST_IDENTIFIER,
+                                         self.TEST_GAME_NAME, Faction.AGRICULTURISTS),
+                            "process_unready_event", with_sock=False)
+        validate_event_type(AutofillEvent(EventType.AUTOFILL, self.TEST_IDENTIFIER, self.TEST_GAME_NAME),
+                            "process_autofill_event")
+        validate_event_type(SaveEvent(EventType.SAVE, self.TEST_IDENTIFIER, self.TEST_GAME_NAME),
+                            "process_save_event", with_sock=False)
+        validate_event_type(QuerySavesEvent(EventType.QUERY_SAVES, self.TEST_IDENTIFIER), "process_query_saves_event")
+        validate_event_type(LoadEvent(EventType.LOAD, self.TEST_IDENTIFIER, "Save 1"), "process_load_event")
+        validate_event_type(Event(EventType.KEEPALIVE, self.TEST_IDENTIFIER),
+                            "process_keepalive_event", with_sock=False)
+
+    @patch("random.choice")
+    def test_process_create_event_server(self, random_choice_mock: MagicMock):
+        """
+        Ensure that the game server correctly processes create events.
+        """
+        test_event: CreateEvent = CreateEvent(EventType.CREATE, self.TEST_IDENTIFIER, self.TEST_GAME_CONFIG)
+        test_faction: Faction = test_event.cfg.player_faction
+        taken_lobby_name: str = LOBBY_NAMES[0]
+        test_lobby_name: str = LOBBY_NAMES[1]
+        test_player_name: str = PLAYER_NAMES[0]
+        self.mock_server.is_server = True
+        # Add in an extra game state so we can see the lobby name iteration process.
+        self.mock_server.game_states_ref = {taken_lobby_name: GameState()}
+        # There are two different calls to random.choice() when processing create events as the server. Firstly, the
+        # lobby name is chosen, and subsequently the player's name is chosen. We mock the method to initially return a
+        # lobby name that's already been taken, thus covering the case where another lobby name needs to be randomly
+        # selected. Our second return value is a valid lobby name, and we finally return a player name.
+        random_choice_mock.side_effect = [taken_lobby_name, test_lobby_name, test_player_name]
+
+        # Process our test event.
+        self.request_handler.process_create_event(test_event, self.mock_socket)
+
+        # The game state should have been created, with the expected Player object.
+        self.assertIn(test_lobby_name, self.mock_server.game_states_ref)
+        self.assertListEqual([Player(test_player_name, test_faction, FACTION_COLOURS[test_faction])],
+                             self.mock_server.game_states_ref[test_lobby_name].players)
+        # A Namer and MoveMaker should have been created for the lobby.
+        self.assertIn(test_lobby_name, self.mock_server.namers_ref)
+        self.assertIn(test_lobby_name, self.mock_server.move_makers_ref)
+        # The player's identifier should have been added to the game clients for this lobby.
+        self.assertIn(test_lobby_name, self.mock_server.game_clients_ref)
+        self.assertListEqual([PlayerDetails(test_player_name, test_faction, test_event.identifier)],
+                             self.mock_server.game_clients_ref[test_lobby_name])
+        # The game's config should have been saved under the lobby name.
+        self.assertIn(test_lobby_name, self.mock_server.lobbies_ref)
+        self.assertEqual(test_event.cfg, self.mock_server.lobbies_ref[test_lobby_name])
+        # We also expect the event itself to have been added to, with the additional information required for the client
+        # to handle the response.
+        self.assertEqual(test_lobby_name, test_event.lobby_name)
+        self.assertEqual(self.mock_server.game_clients_ref[test_lobby_name], test_event.player_details)
+        # Lastly, we expect the server to have sent a packet containing a JSON representation of the event to the client
+        # that originally dispatched the create event.
+        self.mock_socket.sendto.assert_called_with(json.dumps(test_event, cls=SaveEncoder).encode(),
+                                                   (self.TEST_HOST, self.TEST_PORT))
 
 
 if __name__ == '__main__':
